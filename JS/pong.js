801dd66eab9e51ff3a2d44ffbdeb2291c0527731
hns707 <72392569+hns707@users.noreply.github.com>
2020-11-02 16:04:15 +0100

    up

---
+++ JS/pong.js
@@ -0,0 +1,10 @@
+let largeur=$("#balle").width();
+let gauche=parseInt($("#balle").css("left"));
+let haut=parseInt($("#balle").css("top"));
+alert(largeur);
+
+setInterval(fonction(){
+    
+
+}
+)
