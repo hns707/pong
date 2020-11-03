c478a1d7966e95f8e6e416315efe3954e3b7b5c1
HNS <dephestos.yowane@gmail.com>
2020-11-03 21:13:59 +0100

    base

--- JS/pong.js
+++ JS/pong.js
@@ -1,10 +1,15 @@
+//Définir les variables du script
+
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
-alert(largeur);
 
-setInterval(fonction(){
-    
+//Script déplacement de la balle
+setInterval(function(){
+    gauche = gauche + 1;
+    haut = haut + 0.5;
+    //Affiche le résultat de I dans le titre de page toutes les 10 millisecondes 
+    $("#balle").css("left",gauche); 
+    $("#balle").css("top",haut);
 
-}
-)
+}, 10);
