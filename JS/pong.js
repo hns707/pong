aadb32eeeaebe258d8b2e060fda944618b30ab23
HNS <dephestos.yowane@gmail.com>
2020-11-08 17:53:32 +0100

    up

--- JS/pong.js
+++ JS/pong.js
@@ -6,8 +6,10 @@ let haut=parseInt($("#balle").css("top"));
 
 //Script déplacement de la balle
 setInterval(function(){
+	// Vitesse de déplacement
     gauche = gauche + 1;
-    haut = haut + 0.5; 
+    haut = haut + 0.5;
+	// Actualisation du CSS
     $("#balle").css("left",gauche); 
     $("#balle").css("top",haut);
 
