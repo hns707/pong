8fe0358f755e769326ba6b978f697aa77298d7f4
Barbato <72392569+hns707@users.noreply.github.com>
2020-11-12 02:27:04 +0100

    up

--- JS/pong.js
+++ JS/pong.js
@@ -10,6 +10,10 @@ setInterval(function(){
 
     balle.maj(); 
 	
+	raquetteGauche.rebords(), raquetteGauche.majGCSS();
+	raquetteDroite.rebords(), raquetteDroite.majDCSS();
+	
+	
 }, 10);
 
 class Terrain{
@@ -19,6 +23,41 @@ class Terrain{
         this.hauteur=$element.height();
     }
 }
+
+class Raquette{
+    constructor($element){
+        this.$element=$element;
+        this.largeur=$element.width();
+        this.hauteur=$element.height();
+        this.posx=parseInt($element.css("left"));
+        this.posy=parseInt($element.css("top"));
+        this.speedy=2;
+        this.sensy=1;
+	}
+	
+	rebords(){
+		//Collisions terrain
+		 if (this.posy > terrain.hauteur - this.hauteur){
+			this.sensy = -1;
+		 }
+		 else if (this.posy < 0){
+			 this.sensy = 1;
+		 }
+		 this.posy = this.posy + this.speedy * this.sensy;
+	}
+	
+	 majGCSS(){
+        // Actualisation du CSS
+        $("#rgauche").css("left",this.posx); 
+        $("#rgauche").css("top",this.posy);
+	 }
+	 
+	 majDCSS(){
+        // Actualisation du CSS
+        $("#rdroite").css("left",this.posx); 
+        $("#rdroite").css("top",this.posy);
+	 }
+ }
 
 class Balle{
     constructor($element){
@@ -31,9 +70,11 @@ class Balle{
         this.speedy=2
         this.sensx=1;
         this.sensy=1;
-        this.diametre=20;
+        this.diametre=15;
 
     }
+	
+	
     maj(){
         // Vitesse de déplacement
         this.posx = this.posx + this.speedx * this.sensx;
@@ -46,11 +87,11 @@ class Balle{
         if(this.posx > terrain.largeur - this.diametre){
             this.posx = terrain.largeur - this.diametre;
             this.sensx = -1;
-            terrain.$element.addClass("vert");
+            terrain.$element.addClass("rouge");
             setTimeout(
                 function(){
-                    terrain.$element.removeClass("vert")
-                },100
+                    terrain.$element.removeClass("rouge")
+                },1000
             );
             
         }
@@ -58,11 +99,11 @@ class Balle{
         else if(this.posx < 0 ){
             this.posx = 0;
             this.sensx = 1;
-            terrain.$element.addClass("vert");
+            terrain.$element.addClass("rouge");
             setTimeout(
                 function(){
-                    terrain.$element.removeClass("vert")
-                },100
+                    terrain.$element.removeClass("rouge")
+                },1000
             );
         }
     
@@ -92,9 +133,19 @@ class Balle{
     }
 }
 
-//Création d'un terrian + balle
+
+
+//Création d'un terrian + balle + raquettes
 let terrain=new Terrain($("#terrain"));
 let balle=new Balle($("#balle"));
+let raquetteGauche=new Raquette($("#rgauche"));
+let raquetteDroite=new Raquette($("#rdroite"));
+
+// Position X fixe
+raquetteGauche.posx = 20;
+raquetteDroite.posx = terrain.largeur - 20 - raquetteDroite.largeur;
+
+console.log(raquetteGauche.hauteur);
 
 // Position et sens de départ de la balle aléatoire
 balle.posx = Math.random() * terrain.largeur;
@@ -102,4 +153,6 @@ balle.posy = Math.random() * terrain.hauteur;
 balle.sensx = balle.sensx * (Math.random() < 0.5) ? -1 : 1;
 balle.sensy = balle.sensy * (Math.random() < 0.5) ? -1 : 1;
 console.log(terrain);
+
+
 
